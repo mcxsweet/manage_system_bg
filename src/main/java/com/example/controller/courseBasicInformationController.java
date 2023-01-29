e4b61fb0f29f7d2128e1d9df5fa3e8e1c01dc604
aerlany <1243535201@qq.com>
2023-01-29 22:22:49 +0800

    Swagger API Document

--- src/main/java/com/example/controller/courseBasicInformationController.java
+++ src/main/java/com/example/controller/courseBasicInformationController.java
@@ -3,26 +3,30 @@ package com.example.controller;
 import com.example.object.courseBasicInformation;
 import com.example.service.impl.courseBasicInformationServiceIMPL;
 import com.example.utility.DataResponses;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.utility.export.export;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.crypto.Data;
 import java.io.IOException;
 
+@Api(tags="课程信息")
 @RestController
 @RequestMapping("/courseInfo")
 public class courseBasicInformationController {
     @Autowired
     private courseBasicInformationServiceIMPL courseBasicInformationService;
 
+    @ApiOperation("查询全部")
     @GetMapping
     public DataResponses getAll() {
         return new DataResponses(true, courseBasicInformationService.list());
     }
 
     //导出课程基本信息
+    @ApiOperation("导出课程基本信息")
     @GetMapping("/export/{id}")
     public void exportExcel(HttpServletResponse response, @PathVariable int id) throws IOException {
 
@@ -31,11 +35,13 @@ public class courseBasicInformationController {
         export.ExportCourseBasicInformationExcel(response, information);
     }
 
+    @ApiOperation("添加")
     @PostMapping
     public DataResponses write(@RequestBody courseBasicInformation pages) {
         return new DataResponses(courseBasicInformationService.save(pages));
     }
 
+    @ApiOperation("删除")
     @DeleteMapping
     public DataResponses delete(@RequestBody courseBasicInformation pages){
         return new DataResponses(courseBasicInformationService.removeById(pages));
--- src/main/java/com/example/controller/courseExamineMethodsController.java
+++ src/main/java/com/example/controller/courseExamineMethodsController.java
@@ -6,9 +6,12 @@ import com.example.object.courseExamineMethods;
 import com.example.service.impl.courseExamineChildMethodsServiceIMPL;
 import com.example.service.impl.courseExamineMethodsServiceIMPL;
 import com.example.utility.DataResponses;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags="考核与评价方式")
 @RestController
 @RequestMapping("/courseExam")
 public class courseExamineMethodsController {
@@ -21,6 +24,7 @@ public class courseExamineMethodsController {
     private courseExamineChildMethodsServiceIMPL courseExamineChildMethodsService;
 
     //提供课程id查询考核项目
+    @ApiOperation("提供课程id查询考核项目")
     @GetMapping("/courseExamineMethods/{courseId}")
     public DataResponses getCourseExamineMethodsById(@PathVariable int courseId) {
         QueryWrapper<courseExamineMethods> QueryWrapper = new QueryWrapper<>();
@@ -29,6 +33,7 @@ public class courseExamineMethodsController {
     }
 
     //提供考核项目id查询子考核项目
+    @ApiOperation("提供考核项目id查询子考核项目")
     @GetMapping("/courseExamineChildMethods/{courseExamineMethodsId}")
     public DataResponses getCourseExamineChildMethodsById(@PathVariable int courseExamineMethodsId) {
         QueryWrapper<courseExamineChildMethods> QueryWrapper = new QueryWrapper<>();
@@ -37,12 +42,14 @@ public class courseExamineMethodsController {
     }
 
     //提供课程id添加考核项目
+    @ApiOperation("提供课程id添加考核项目")
     @PostMapping("/courseExamineMethods")
     public DataResponses addCourseExamineMethods(@RequestBody courseExamineMethods item) {
         return new DataResponses(courseExamineMethodsService.save(item));
     }
 
     //提供考核项目id添加子考核项目
+    @ApiOperation("提供考核项目id添加子考核项目")
     @PostMapping("/courseExamineChildMethods")
     public DataResponses addCourseExamineChildMethods(@RequestBody courseExamineChildMethods item) {
         return new DataResponses(courseExamineChildMethodsService.save(item));
