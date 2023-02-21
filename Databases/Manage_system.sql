1ba020441775c67436919abe5ab541c3681684d2
aerlany <1243535201@qq.com>
2023-02-21 15:43:07 +0800

    数据库更新操作

--- Databases/Manage_system.sql
+++ Databases/Manage_system.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.40, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.41, for Linux (x86_64)
 --
 -- Host: localhost    Database: Manage_system
 -- ------------------------------------------------------
--- Server version	5.7.40
+-- Server version	5.7.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -46,7 +46,7 @@ CREATE TABLE `course_basic_information` (
 
 LOCK TABLES `course_basic_information` WRITE;
 /*!40000 ALTER TABLE `course_basic_information` DISABLE KEYS */;
-INSERT INTO `course_basic_information` VALUES (1,'高数','阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(2,'线性代数','王老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,3,'指标1,指标2,指标3'),(4,'概率论','阳老师1222',16123,41231,'计算机科学与技术2022','2022-2023.1',80111,'shanghai','专业必修课',5,2,'指标1,指标2'),(6,'c语言程序设计','阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2');
+INSERT INTO `course_basic_information` VALUES (1,'高数(必修)','阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(2,'线性代数','王老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,3,'指标1,指标2,指标3'),(4,'概率论','阳老师1222',16123,41231,'计算机科学与技术2022','2022-2023.1',80111,'shanghai','专业必修课',5,2,'指标1,指标2'),(6,'c语言程序设计','阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2');
 /*!40000 ALTER TABLE `course_basic_information` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,6 +108,63 @@ LOCK TABLES `course_examine_methods` WRITE;
 INSERT INTO `course_examine_methods` VALUES (1,1,'高数','考试',70),(2,1,'高数','平时测验',30),(3,2,'线性代数','考试',70),(4,2,'线性代数','平时测试',30);
 /*!40000 ALTER TABLE `course_examine_methods` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `course_target`
+--
+
+DROP TABLE IF EXISTS `course_target`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course_target` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(30) NOT NULL,
+  `course_target` text NOT NULL,
+  `path_ways` text NOT NULL,
+  `indicator_points` text NOT NULL,
+  `evaluation_method` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course_basic_information_id` (`course_id`),
+  CONSTRAINT `course_basic_information_id` FOREIGN KEY (`course_id`) REFERENCES `course_basic_information` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_target`
+--
+
+LOCK TABLES `course_target` WRITE;
+/*!40000 ALTER TABLE `course_target` DISABLE KEYS */;
+INSERT INTO `course_target` VALUES (1,1,'高数','实现基本操作sdfsdf','考试','指标点1.1','考试'),(2,1,'高数','理解信号的特征、分类，理解系统的描述和分类，掌握信号的运算和连续时不变系统的性质和判断；','通过课堂讲授、作业和考研题探讨方式，让学生理解和掌握信号、系统性质和运算。','指标点1.1:\n掌握数学、自然科学、工程基础和电子信息专业知识的基本概念，并将其运用到复杂工程问题的适当表述之中','考试'),(4,6,'c语言程序设计','aaaaaaaaaaadasdasd','1232dadXzcscascdfsdasadsadasdad','[\"选项1\",\"选项2\"]','[\"考试\",\"作业\"]'),(5,4,'概率论','wdaeasdasdasd','asdasd','[\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(6,6,'c语言程序设计','edadsadaczcweqwedad阿达是大大','大赛大赛大大','[\"选项1\",\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(8,2,'线性代数','阿三打是大势打豆豆','是大大是大大的','[\"选项1\",\"选项2\",\"选项4\"]','[\"考试\",\"作业\"]');
+/*!40000 ALTER TABLE `course_target` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `is_admin` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'阳光伟','123456',0),(2,'admin','123',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -118,4 +175,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-02 22:20:55
+-- Dump completed on 2023-02-21 15:42:07
