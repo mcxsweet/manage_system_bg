ba60ecb67551f787f270b52f369c98eb71540b27
aerlany <1243535201@qq.com>
2023-03-02 19:12:24 +0800

    数据库变更

--- Databases/Manage_system.sql
+++ Databases/Manage_system.sql
@@ -37,8 +37,10 @@ CREATE TABLE `course_basic_information` (
   `course_target_num` int(3) NOT NULL,
   `indicator_points_num` int(3) NOT NULL,
   `indicator_points` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `课程基本信息表与用户表id关联` (`teacher_id`),
+  CONSTRAINT `课程基本信息表与用户表id关联` FOREIGN KEY (`teacher_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +49,7 @@ CREATE TABLE `course_basic_information` (
 
 LOCK TABLES `course_basic_information` WRITE;
 /*!40000 ALTER TABLE `course_basic_information` DISABLE KEYS */;
-INSERT INTO `course_basic_information` VALUES (1,'高数(必修)',1,'阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(2,'线性代数',1,'王老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,3,'指标1,指标2,指标3'),(4,'概率论',1,'阳老师1222',16123,41231,'计算机科学与技术2022','2022-2023.1',80111,'必修','专业必修课',5,2,'指标1,指标2'),(6,'c语言程序设计',1,'阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(7,'JAVA程序设计',1,'阳老师',1231,132,'计算,机科学与技术2020','123',123,'必修','专业必修课',5,1231,'指标点1');
+INSERT INTO `course_basic_information` VALUES (1,'高数(必修)',1,'阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(2,'线性代数',1,'王老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,3,'指标1,指标2,指标3'),(4,'概率论',1,'阳老师1222',16123,41231,'计算机科学与技术2022','2022-2023.1',80111,'必修','专业必修课',5,2,'指标1,指标2'),(6,'c语言程序设计',1,'阳老师',16,4,'计算机科学与技术2020','2022-2023.1',80,'必修','专业必修课',5,2,'指标1,指标2'),(7,'JAVA程序设计',1,'阳老师',1231,132,'计算,机科学与技术2020','123',123,'必修','专业必修课',5,1231,'指标点1'),(8,'高数',3,'admin2',12,12,'计算机科学与技术2020','2020-2021,1',122,'必修','公共必修',4,1,'指标点1.1');
 /*!40000 ALTER TABLE `course_basic_information` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,7 +70,7 @@ CREATE TABLE `course_examine_child_methods` (
   PRIMARY KEY (`id`),
   KEY `考试方式表id` (`course_examine_methods_id`),
   CONSTRAINT `考试方式表id` FOREIGN KEY (`course_examine_methods_id`) REFERENCES `course_examine_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,7 +79,7 @@ CREATE TABLE `course_examine_child_methods` (
 
 LOCK TABLES `course_examine_child_methods` WRITE;
 /*!40000 ALTER TABLE `course_examine_child_methods` DISABLE KEYS */;
-INSERT INTO `course_examine_child_methods` VALUES (4,2,'平时作业',50,'{\'课程目标1\':\'是\',\'课程目标2\':\'是\',\'课程目标3\':\'是\',\'课程目标4\':\'是\',\'课程目标5\':\'是\'}','{\'指标点1.1\':\'是\',\'指标点1.2\':\'是\',\'指标点1.3\':\'是\'}'),(5,2,'考勤',50,'{\'课程目标1\':\'是\',\'课程目标2\':\'是\',\'课程目标3\':\'是\',\'课程目标4\':\'是\',\'课程目标5\':\'是\'}','{\'指标点1.1\':\'是\',\'指标点1.2\':\'是\',\'指标点1.3\':\'是\'}'),(6,1,'期末测试',50,'{\'课程目标1\':\'是\',\'课程目标2\':\'是\',\'课程目标3\':\'是\',\'课程目标4\':\'是\',\'课程目标5\':\'是\'}','{\'指标点1.1\':\'是\',\'指标点1.2\':\'是\',\'指标点1.3\':\'是\'}'),(7,1,'期中测试',50,'{\'课程目标1\':\'是\',\'课程目标2\':\'是\',\'课程目标3\':\'是\',\'课程目标4\':\'是\',\'课程目标5\':\'是\'}','{\'指标点1.1\':\'是\',\'指标点1.2\':\'是\',\'指标点1.3\':\'是\'}'),(8,3,'期中测试',50,NULL,NULL);
+INSERT INTO `course_examine_child_methods` VALUES (10,3,'平时考核成绩',8,'[\"课程目标1\",\"课程目标2\"]','[\"指标点1\",\"指标点2\"]'),(11,1,'平时考核成绩',23,'[\"课程目标1\",\"课程目标2\"]','[\"指标点1\",\"指标点2\"]'),(13,1,'实验考核成绩',12,'[\"课程目标1\"]','[\"指标点1\"]');
 /*!40000 ALTER TABLE `course_examine_child_methods` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +99,7 @@ CREATE TABLE `course_examine_methods` (
   PRIMARY KEY (`id`),
   KEY `课程信息表id` (`course_id`),
   CONSTRAINT `课程信息表id` FOREIGN KEY (`course_id`) REFERENCES `course_basic_information` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,8 +108,67 @@ CREATE TABLE `course_examine_methods` (
 
 LOCK TABLES `course_examine_methods` WRITE;
 /*!40000 ALTER TABLE `course_examine_methods` DISABLE KEYS */;
-INSERT INTO `course_examine_methods` VALUES (1,1,'高数','考试',70),(2,1,'高数','平时测验',30),(3,2,'线性代数','考试',70),(4,2,'线性代数','平时测试',30);
+INSERT INTO `course_examine_methods` VALUES (1,1,'高数(必修)','考试',69),(3,2,'线性代数','考试',70),(4,2,'线性代数','平时测试',30),(5,7,'JAVA程序设计','平时考核成绩',3),(7,6,'c语言程序设计','平时考核成绩',6),(8,6,'c语言程序设计','平时考核成绩',6),(9,6,'c语言程序设计','平时考核成绩',6),(15,4,'概率论','实验考核成绩',412),(17,2,'线性代数','平时考核成绩',4);
 /*!40000 ALTER TABLE `course_examine_methods` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_final_exam_paper`
+--
+
+DROP TABLE IF EXISTS `course_final_exam_paper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course_final_exam_paper` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `item_name` varchar(30) NOT NULL,
+  `item_score` int(3) DEFAULT NULL,
+  `type` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `course_final_exam_paper_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course_basic_information` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_final_exam_paper`
+--
+
+LOCK TABLES `course_final_exam_paper` WRITE;
+/*!40000 ALTER TABLE `course_final_exam_paper` DISABLE KEYS */;
+INSERT INTO `course_final_exam_paper` VALUES (1,1,'选择题',20,0),(3,1,'填空题',30,0),(4,1,'实验',100,1);
+/*!40000 ALTER TABLE `course_final_exam_paper` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_final_exam_paper_detail`
+--
+
+DROP TABLE IF EXISTS `course_final_exam_paper_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course_final_exam_paper_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `primary_id` int(11) NOT NULL,
+  `title_number` varchar(10) NOT NULL,
+  `score` int(100) NOT NULL,
+  `indicator_points` varchar(100) NOT NULL,
+  `course_target` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `与课程试卷表id外联` (`primary_id`),
+  CONSTRAINT `与课程试卷表id外联` FOREIGN KEY (`primary_id`) REFERENCES `course_final_exam_paper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_final_exam_paper_detail`
+--
+
+LOCK TABLES `course_final_exam_paper_detail` WRITE;
+/*!40000 ALTER TABLE `course_final_exam_paper_detail` DISABLE KEYS */;
+INSERT INTO `course_final_exam_paper_detail` VALUES (1,1,'1',2,'[指标点1.1,指标点1.2]','[课程目标1,课程目标2]');
+/*!40000 ALTER TABLE `course_final_exam_paper_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -128,7 +189,7 @@ CREATE TABLE `course_target` (
   PRIMARY KEY (`id`),
   KEY `course_basic_information_id` (`course_id`),
   CONSTRAINT `course_basic_information_id` FOREIGN KEY (`course_id`) REFERENCES `course_basic_information` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,7 +198,7 @@ CREATE TABLE `course_target` (
 
 LOCK TABLES `course_target` WRITE;
 /*!40000 ALTER TABLE `course_target` DISABLE KEYS */;
-INSERT INTO `course_target` VALUES (4,6,'c语言程序设计','aaaaaaaaaaadasdasd','1232dadXzcscascdfsdasadsadasdad','[\"选项1\",\"选项2\"]','[\"考试\",\"作业\"]'),(5,4,'概率论','wdaeasdasdasd','asdasd','[\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(6,6,'c语言程序设计','edadsadaczcweqwedad阿达是大大','大赛大赛大大','[\"选项1\",\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(8,2,'线性代数','阿三打是大势打豆豆','是大大是大大的','[\"选项1\",\"选项2\",\"选项4\"]','[\"考试\",\"作业\"]'),(9,1,'高数(必修)','阿斯顿焚膏继晷语序虚词是大扫除','asadadadadaxzcvvdffgdg','[\"选项1\",\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]');
+INSERT INTO `course_target` VALUES (4,6,'c语言程序设计','aaaaaaaaaaadasdasd','1232dadXzcscascdfsdasadsadasdad','[\"选项1\",\"选项2\"]','[\"考试\",\"作业\"]'),(5,4,'概率论','wdaeasdasdasd','asdasd','[\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(6,6,'c语言程序设计','edadsadaczcweqwedad阿达是大大','大赛大赛大大','[\"选项1\",\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(8,2,'线性代数','阿三打是大势打豆豆','是大大是大大的','[\"选项1\",\"选项2\",\"选项4\"]','[\"考试\",\"作业\"]'),(9,1,'高数(必修)','阿斯顿焚膏继晷语序虚词是大扫除','asadadadadaxzcvvdffgdg','[\"选项1\",\"选项2\",\"选项3\",\"选项4\",\"选项5\"]','[\"考试\",\"作业\"]'),(10,8,'高数','asdadadada','adsadad','[\"选项1\",\"选项2\",\"选项3\",\"选项4\"]','[\"考试\",\"作业\"]');
 /*!40000 ALTER TABLE `course_target` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,10 +212,10 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(20) DEFAULT '123456',
   `is_admin` int(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,7 +224,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','123',1);
+INSERT INTO `user` VALUES (1,'admin','123',1),(3,'admin2','123',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -176,4 +237,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-24 16:41:33
+-- Dump completed on 2023-03-02 19:11:54
