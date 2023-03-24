d6e881a7316bb97c82d5515a248d9d1a2122b669
aerlany <1243535201@qq.com>
2023-03-24 16:56:39 +0800

    用户登录接口添加token获取

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.object.User;
 import com.example.service.impl.UserServiceIMPL;
 
@@ -11,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 
 
 @Api(tags = "用户登录")
@@ -22,11 +22,13 @@ public class UserController {
 
     @ApiOperation("登录接口")
     @PostMapping
-    public DataResponses submit(@RequestBody User data){
-        QueryWrapper<User> QueryWrapper = new QueryWrapper<>();
-        QueryWrapper.eq("name",data.getName()).eq("password",data.getPassword());
-        User user = userService.getOne(QueryWrapper);
-        return new DataResponses(user != null,user);
+    public DataResponses submit(@RequestBody User user, HttpServletResponse response){
+//        QueryWrapper<User> QueryWrapper = new QueryWrapper<>();
+//        QueryWrapper.eq("name",data.getName()).eq("password",data.getPassword());
+//        User user = userService.getOne(QueryWrapper);
+//        return new DataResponses(user != null,user);
+
+        return userService.loginCheck(user, response);
     }
 
     @ApiOperation("按id修改")
