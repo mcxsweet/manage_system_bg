e2a6aeb1857a2be1ac6867c6b751bb673d4cc817
aerlany <1243535201@qq.com>
2023-03-24 16:56:39 +0800

    用户登录接口添加token获取

--- src/main/java/com/example/service/impl/UserServiceIMPL.java
+++ src/main/java/com/example/service/impl/UserServiceIMPL.java
@@ -1,11 +1,46 @@
 package com.example.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.mapper.UserMAPPER;
 import com.example.object.User;
 import com.example.service.UserSERVICE;
+import com.example.utility.DataResponses;
+import com.example.utility.Token.TokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 public class UserServiceIMPL extends ServiceImpl<UserMAPPER, User> implements UserSERVICE {
+
+    @Autowired
+    private UserMAPPER userMAPPER;
+
+    @Autowired
+    TokenUtil tokenUtil;
+
+    @Override
+    public DataResponses loginCheck(User user, HttpServletResponse response) {
+
+        QueryWrapper<User> QueryWrapper = new QueryWrapper<>();
+        QueryWrapper.eq("name",user.getName());
+        User user2 = userMAPPER.selectOne(QueryWrapper);
+        if (user2 == null) {
+            return new DataResponses(false,"用户不存在");
+        }
+        if (!user2.getPassword().equals(user.getPassword())) {
+            return new DataResponses(false,"密码错误");
+        }
+        String token = tokenUtil.generateToken(user2);
+        Cookie cookie = new Cookie("token", token);
+//        设置cookie的作用域：为”/“时，以在webapp文件夹下的所有应用共享cookie
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return new DataResponses(true,"登录成功");
+    }
+
+
 }
