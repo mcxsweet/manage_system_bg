5e8cf013790372d2ebf72f9c8a031d95e508efc3
aerlany <1243535201@qq.com>
2023-04-16 15:21:17 +0800

    平时成绩模块

--- src/test/java/com/example/manage_system_bg/ManageSystemBgApplicationTests.java
+++ src/test/java/com/example/manage_system_bg/ManageSystemBgApplicationTests.java
@@ -1,19 +1,52 @@
 package com.example.manage_system_bg;
 
+import com.example.mapper.UserMAPPER;
+import com.example.object.User;
 import com.example.service.impl.examinePaper.CourseFinalExamPaperDetailServiceIMPL;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootTest
 class ManageSystemBgApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
     @Autowired
     private CourseFinalExamPaperDetailServiceIMPL courseFinalExamPaperDetailServiceIMPL;
+
+    @Autowired
+    private UserMAPPER userMAPPER;
+
+    @Test
+    void contextLoads() throws BiffException, IOException {
+        Workbook workbook = Workbook.getWorkbook(new File("/home/user/Documents/项目/Manage_system_bg/src/main/resources/static/课程达成度评价管理系统教师账号.xls"));
+        Sheet sheet = workbook.getSheet(0);
+        for (int i = 2; i < sheet.getRows(); i++) {
+            // 获取第一列的第 i 行信息 sheet.getCell(列，行)，下标从0开始
+            int id = Integer.parseInt(sheet.getCell(0, i).getContents());
+            // 获取第二列的第 i 行信息
+            String teacherName = sheet.getCell(1, i).getContents();
+            String department = sheet.getCell(2, i).getContents();
+            String name = sheet.getCell(3, i).getContents();
+            String password = sheet.getCell(4, i).getContents();
+
+            User user = new User();
+            user.setName(name);
+            user.setPassword(password);
+            user.setDepartment(department);
+            user.setTeacherName(teacherName);
+
+            userMAPPER.insert(user);
+
+        }
+    }
+
+
 
 
 }
