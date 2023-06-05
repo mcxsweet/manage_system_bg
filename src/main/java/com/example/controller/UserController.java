8b6c1cbac9058dcbf3351e973d3e88d4b2eeafca
蛐蛐 <2950214626@qq.com>
2023-06-05 17:09:11 +0800

    权限重构

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.example.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.example.object.User;
 import com.example.service.impl.UserServiceIMPL;
-
 import com.example.utility.DataResponses;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +18,19 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController {
     @Autowired
     private UserServiceIMPL userService;
+
+    // 会话登录接口
+    @RequestMapping("/doLogin")
+    public DataResponses doLogin(@RequestBody User user) {
+        return userService.login(user);
+    }
+
+    // 查询登录信息 登录信息可以不存在前端，每次需通过后端验证获取
+    @RequestMapping("/info")
+    public DataResponses isLogin() {
+        Object o = StpUtil.getSession().get(StpUtil.getLoginIdAsString());
+        return new DataResponses(true, o);
+    }
 
     @ApiOperation("登录接口")
     @PostMapping
