0e48afb2896ae881991e469f6abf1b5ba2a7c752
aerlany <1243535201@qq.com>
2023-06-20 14:19:29 +0800

    课程满意度调查相关接口

---
+++ src/main/java/com/example/mapper/courseSurvey/CourseAttainmentSurveyMAPPER.java
@@ -0,0 +1,10 @@
+package com.example.mapper.courseSurvey;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.object.courseSurvey.CourseAttainmentSurvey;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CourseAttainmentSurveyMAPPER extends BaseMapper<CourseAttainmentSurvey> {
+//    在此出写拓展sql
+}
---
+++ src/main/java/com/example/object/courseSurvey/CourseAttainmentSurvey.java
@@ -0,0 +1,14 @@
+package com.example.object.courseSurvey;
+
+import lombok.Data;
+
+@Data
+public class CourseAttainmentSurvey {
+
+    //课程目标id
+    private int courseTargetId;
+    //满意度等级
+    private int studentId;
+    private int attainment;
+
+}
