249617b05cc1ebf15bf7eeaccadfecca26ab2832
aerlany <1243535201@qq.com>
2023-07-05 11:56:35 +0800

    教学大纲,培养方案上传,预览,删除相关接口完成

---
+++ src/main/java/com/example/mapper/CourseSyllabusInformationMAPPER.java
@@ -0,0 +1,10 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.object.CourseSyllabusInformation;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CourseSyllabusInformationMAPPER extends BaseMapper<CourseSyllabusInformation> {
+//    在此出写拓展sql
+}
---
+++ src/main/java/com/example/object/CourseSyllabusInformation.java
@@ -0,0 +1,32 @@
+package com.example.object;
+
+import lombok.Data;
+
+@Data
+public class CourseSyllabusInformation {
+    private Integer id;
+    //课程名称
+    private String courseName;
+    //课程代码
+    private String courseCode;
+    //专业
+    private String major;
+    //学分
+    private Double credit;
+    //理论时长
+    private int theoreticalHours;
+    //实验学识
+    private int labHours;
+    //课程性质
+    private String courseNature;
+    //课程类别
+    private String courseType;
+    //暂不设置
+//    //课程目标数量
+//    private int courseTargetNum;
+//    //指标点数量
+//    private int indicatorPointsNum;
+    //pdf文件地址
+    private String fileAddress;
+
+}
