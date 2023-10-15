11362a3ddab9891810767a9fd4ed693952b196da
Favian <1978699474@qq.com>
2023-10-15 18:37:22 +0800

    添加和修改了对于毕业要求指标大纲和指标点的CRUD

---
+++ src/main/java/com/example/service/impl/IndicatorOutlineSERVICEIMPL.java
@@ -0,0 +1,12 @@
+package com.example.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.mapper.IndicatorOutlineMAPPER;
+import com.example.object.IndicatorOutline;
+import com.example.service.IndicatorOutlineSERVICE;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndicatorOutlineSERVICEIMPL extends ServiceImpl<IndicatorOutlineMAPPER, IndicatorOutline> implements IndicatorOutlineSERVICE {
+
+}
